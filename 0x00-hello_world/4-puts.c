35c2f437dc31cc884ce6fe7d43d0cbe2975b4c7c
Lucky Ejike <ejikelucky3@gmail.com>
2023-02-17 07:59:25 -0800

    4-puts.c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+/**
+ * main - A program that print puts functions
+ * Return: 0 (success)
+ */
+int main(void)
+{
+puts("\"Programming is like building a multilingual puzzle);
+return (0);
